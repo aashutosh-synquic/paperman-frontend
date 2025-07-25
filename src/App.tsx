75da7956cf08d7727cb093ae61e1755f078b05de
Aashutosh Soni <44861896+ashutosh7i@users.noreply.github.com>
2025-07-25 19:47:08 +0530

    Add: shadcn ui library.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+"use client";
 
-function App() {
-  const [count, setCount] = useState(0)
+import * as React from "react";
+
+import { Progress } from "@/components/ui/progress";
+import { Button } from "./components/ui/button";
+
+export default function App() {
+  const [progress, setProgress] = React.useState(13);
+
+  React.useEffect(() => {
+    const timer = setTimeout(() => setProgress(66), 500);
+    return () => clearTimeout(timer);
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Test</h1>
+      <Button variant={"default"}>Click Me</Button>
+      <br />
+      <Button variant={"destructive"}>Click to Cancel</Button>
+      <h2>Progress Bar</h2>
+      <Progress value={progress} className="w-[60%]" />
     </>
-  )
+  );
 }
-
-export default App
